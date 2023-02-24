7104ff46eaa55818f8b8e19e81484aabd674888a
Side Account <124309847+Candorada2@users.noreply.github.com>
2023-02-24 19:27:06 +0100

    Update getDescendants.js

--- getDescendants.js
+++ getDescendants.js
@@ -1,5 +1,4 @@
-var holder = document.querySelector("body > div.app-layout > div.home-container > div > div:nth-child(3) > div")
-function getDescendants(obj){
+export function getDescendants(obj){
   var d = new Array()
 function temp(tempObj){
     if(tempObj?.children.length > 0){
@@ -14,4 +13,3 @@ function temp(tempObj){
 }  
     return temp(obj)
 }
-          getDescendants(holder)
